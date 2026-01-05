8ff85a876e4bc21ba6eec9868572a05ab9213424
seucaio <caiopimentel8@gmail.com>
2026-01-05 01:47:12 -0300

    feat: Relocate theme files to `core:design-system` module
    This commit refactors the project by moving the Jetpack Compose theme-related files (`Color.kt`, `Theme.kt`, `Type.kt`) from the `:app` module to the new `:core:design-system` module.
    
    The `:app` module now depends on `:core:design-system` and `:core:common` to access the shared theme and utilities. This change centralizes the design system for better modularity and reusability.

--- app/src/main/java/br/com/seucaio/pokeguess/MainActivity.kt
+++ app/src/main/java/br/com/seucaio/pokeguess/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import br.com.seucaio.pokeguess.ui.theme.PokeGuessTheme
+import br.com.seucaio.pokeguess.core.designsystem.ui.theme.PokeGuessTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
