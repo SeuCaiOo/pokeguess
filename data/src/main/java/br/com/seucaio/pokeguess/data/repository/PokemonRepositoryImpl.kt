8910cec304e29a905437605ce195a62118285028
seucaio <caiopimentel8@gmail.com>
2026-01-10 03:11:32 -0300

    feat: Implement local data source and caching in `PokemonRepository`
    This commit introduces a local data source to the data layer and implements a caching strategy within `PokemonRepositoryImpl`. The repository now prioritizes local data and falls back to the remote source when the cache is empty.
    
    Key changes include:
    - Created `PokemonLocalDataSource` and its implementation to handle Room database operations via `PokemonDao`.
    - Updated `PokemonRepositoryImpl` to include `PokemonLocalDataSource`, implementing logic to fetch, cache, and clear Pokémon data.
    - Added `getPokemonById` to the `PokemonRepository` interface and its implementation to retrieve specific Pokémon from the local database.
    - Integrated Mappers to handle conversions between `PokemonEntity`, `Pokemon` domain models, and API responses.
    - Configured `PokemonLocalDataSourceImpl` to use `Dispatchers.IO` for database operations.

--- data/src/main/java/br/com/seucaio/pokeguess/data/repository/PokemonRepositoryImpl.kt
+++ data/src/main/java/br/com/seucaio/pokeguess/data/repository/PokemonRepositoryImpl.kt
@@ -1,16 +1,45 @@
 package br.com.seucaio.pokeguess.data.repository
 
+import br.com.seucaio.pokeguess.data.local.database.entity.PokemonEntity
+import br.com.seucaio.pokeguess.data.local.source.PokemonLocalDataSource
+import br.com.seucaio.pokeguess.data.mapper.PokemonMapper.toDomainList
+import br.com.seucaio.pokeguess.data.mapper.PokemonMapper.toEntityList
+import br.com.seucaio.pokeguess.data.mapper.PokemonMapper.toPokemonDomain
 import br.com.seucaio.pokeguess.data.mapper.PokemonMapper.toPokemonDomainList
+import br.com.seucaio.pokeguess.data.remote.dto.PokemonListResponse
 import br.com.seucaio.pokeguess.data.remote.source.PokemonRemoteDataSource
 import br.com.seucaio.pokeguess.domain.model.Generation
 import br.com.seucaio.pokeguess.domain.model.Pokemon
 import br.com.seucaio.pokeguess.domain.repository.PokemonRepository
+import kotlin.collections.isNotEmpty
 
 internal class PokemonRepositoryImpl(
-    private val remoteDataSource: PokemonRemoteDataSource
+    private val remoteDataSource: PokemonRemoteDataSource,
+    private val localDataSource: PokemonLocalDataSource
 ) : PokemonRepository {
     override suspend fun getPokemons(generation: Generation): List<Pokemon> {
+        return getSuccessListPokemon(generation)
+    }
+
+    private suspend fun getSuccessListPokemon(generation: Generation): List<Pokemon> {
+        val localPokemons: List<PokemonEntity> = localDataSource.getAllPokemons()
+        if (localPokemons.isNotEmpty()) return localPokemons.toDomainList()
+
+        val remotePokemons: List<Pokemon> = getRemotePokemons(generation).toPokemonDomainList()
+        saveCachePokemons(remotePokemons)
+
+        return remotePokemons
+    }
+
+    private suspend fun getRemotePokemons(generation: Generation): PokemonListResponse {
         return remoteDataSource.getPokemons(offset = generation.offset, limit = generation.limit)
-            .toPokemonDomainList()
+    }
+
+    private suspend fun saveCachePokemons(results: List<Pokemon>) {
+        localDataSource.clearAndCachePokemons(results.toEntityList())
+    }
+
+    override suspend fun getPokemonById(pokemonId: Int): Pokemon? {
+        return localDataSource.getByPokemonId(pokemonId)?.toPokemonDomain()
     }
 }
