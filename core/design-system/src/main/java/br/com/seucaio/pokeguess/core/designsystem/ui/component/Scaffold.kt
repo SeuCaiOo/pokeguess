19df8d98601d6d0ad90115a615022557992114bd
seucaio <caiopimentel8@gmail.com>
2026-01-16 12:54:38 -0300

    feat: Add `PokeGuessScaffold` component to `core:design-system`
    This commit introduces a custom `PokeGuessScaffold` component within the `:core:design-system` module. This component simplifies the creation of screens with a consistent layout structure.
    
    Key features include:
    - A standardized layout using `Scaffold` with predefined padding and background colors.
    - Slot-based architecture with `topAppBar`, `topContent`, `centerContent`, and `bottomContent` parameters.
    - Automatic centering of content within the `centerContent` area, which occupies the remaining vertical space.
    - Default horizontal alignment set to `CenterHorizontally` for all content sections.

---
+++ core/design-system/src/main/java/br/com/seucaio/pokeguess/core/designsystem/ui/component/Scaffold.kt
@@ -0,0 +1,53 @@
+package br.com.seucaio.pokeguess.core.designsystem.ui.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun PokeGuessScaffold(
+    centerContent: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    topAppBar: @Composable () -> Unit = {},
+    topContent: @Composable () -> Unit = {},
+    bottomContent: @Composable () -> Unit = {},
+) {
+    Scaffold(
+        modifier = modifier.fillMaxSize(),
+        containerColor = MaterialTheme.colorScheme.background,
+        topBar = { topAppBar() }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .padding(innerPadding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                topContent()
+            }
+            Column(
+                modifier = Modifier.weight(1f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                centerContent()
+            }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                bottomContent()
+            }
+        }
+    }
+}
